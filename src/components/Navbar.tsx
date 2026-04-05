f98929cc9a38654a2dc03b6236837ab6f76ff833
Owen Braun <obraun27@gmail.com>
2026-04-05 15:09:35 +0200

    Basic format updated

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h3 className="navbar-title">Elevate Yourself Coaching</h3>
+        </nav>
+    )
+}
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import './Homepage.css'
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <h2>Welcome to Elevate Yourself Coaching!</h2>
+            <h2>Empowering you to reach your full potential, coming soon!</h2>
+        </div>
+    )
+}
